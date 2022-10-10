511c2a27395e78b4de0b891d7e12be6321bda684
Derrick Heindel <dheindel@overdrive.com>
2022-10-10 09:27:22 -0400

    brew: fix gnu-sed install

--- brew.sh
+++ brew.sh
@@ -15,7 +15,7 @@ brew install moreutils
 # GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed
 brew install findutils
 # GNU `sed`, overwriting the built-in `sed`
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 
 # Help nav the file system
 brew install z
